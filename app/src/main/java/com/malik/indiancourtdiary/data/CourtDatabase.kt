692789e9c0976f94578cf503c49e26fdbf3a2d8d
AamirMalikXCoder <157962080+AamirMalikXCoder@users.noreply.github.com>
2026-08-18 00:05:51 +0530

    Upgrade Room for hearing history

--- app/src/main/java/com/malik/indiancourtdiary/data/CourtDatabase.kt
+++ app/src/main/java/com/malik/indiancourtdiary/data/CourtDatabase.kt
@@ -3,8 +3,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-@Database(entities=[CourtCase::class],version=1,exportSchema=false)
+@Database(entities=[CourtCase::class],version=2,exportSchema=false)
 abstract class CourtDatabase:RoomDatabase(){
  abstract fun courtCaseDao():CourtCaseDao
- companion object{fun create(c:Context)=Room.databaseBuilder(c,CourtDatabase::class.java,"court-diary.db").build()}
+ companion object{fun create(c:Context)=Room.databaseBuilder(c,CourtDatabase::class.java,"court-diary.db").fallbackToDestructiveMigration().build()}
 }
