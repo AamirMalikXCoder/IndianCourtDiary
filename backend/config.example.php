0e312df3b84558683f17818f60e7dd11a79083f4
AamirMalikXCoder <157962080+AamirMalikXCoder@users.noreply.github.com>
2026-08-17 23:48:24 +0530

    Add backend config template

---
+++ backend/config.example.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'ecourts_token' => 'PASTE_TOKEN_ONLY_ON_SERVER',
+    'cache_dir' => __DIR__ . '/storage/cache',
+    'cache_ttl_seconds' => 21600,
+    'allowed_origin' => '*',
+];
