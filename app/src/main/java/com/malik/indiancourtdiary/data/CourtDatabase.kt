caa0144ffce6de0846d530fb9302d68c767d8af0
AamirMalikXCoder <157962080+AamirMalikXCoder@users.noreply.github.com>
2026-08-17 23:41:35 +0530

    Add Room database

---
+++ app/src/main/java/com/malik/indiancourtdiary/data/CourtDatabase.kt
@@ -0,0 +1,10 @@
+package com.malik.indiancourtdiary.data
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+@Database(entities=[CourtCase::class],version=1,exportSchema=false)
+abstract class CourtDatabase:RoomDatabase(){
+ abstract fun courtCaseDao():CourtCaseDao
+ companion object{fun create(c:Context)=Room.databaseBuilder(c,CourtDatabase::class.java,"court-diary.db").build()}
+}
